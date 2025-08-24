50d7301a43dc3257c758463c4510732a984acc41
Yasitha Miyuranga <yasithamiyuranga5@gmail.com>
2025-08-24 23:47:10 +0530

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, {Application} from "express";
+
+
+
+const app:Application = express();
+
+app.use(express.json());
+
+app.get("/",(req, res) =>{
+    res.send("Hello Woeld");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
